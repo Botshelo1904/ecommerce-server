c926ef8f9992638af22240053af6267e2da2ea8a
Botshelo Mabuela <Mabuela408@gmail.com>
2025-08-28 09:22:07 +0200

    Updated MySQL env vars for Clever cloud

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 
 connection.connect((err) => {
